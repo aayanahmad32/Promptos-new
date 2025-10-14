b5163628b124c59ba7aac131f923de3750201b98
aayanahmad32 <aayan.ahmad004@gmail.com>
2025-10-14 12:22:37 +0530

    v1.0.4

--- sw.js
+++ sw.js
@@ -5,34 +5,22 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
-// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-    .catch(() => {
-      // Ignore cache errors
-    })
+    .then(cache => cache.addAll(urlsToCache))
+    .catch(() => {})
   );
   self.skipWaiting();
 });
 
-// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-    .then(response => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
+    .then(response => response || fetch(event.request))
   );
 });
 
-// Activate event
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
